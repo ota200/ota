2f4ec42b4829602c60f4f3c7cfdd510ed14c90d9
O.T.A <>
2021-05-16 22:47:26 +0000

    mobile

---
+++ new.js
@@ -0,0 +1,15 @@
+
+
+function show_hide() {
+	var m = document.getElementById("menu");
+
+	if (m.style.display === "inline-block") {
+		m.style.display = "none";
+	}
+
+	else {
+		m.style.display = "inline-block";
+	}
+}
+
+
