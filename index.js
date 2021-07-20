1e73aff991ebdcc1e45f00c70136b4660214c814
ota200 <aniscratch@gmail.com>
2021-07-20 13:20:44 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
-
-
 function about(){
     window.location.href='about.html'
+}
+
+function projects(){
+    window.location.href='projects.html'
 }
 
 function index(){
     window.location.href='index.html'
 }
+
